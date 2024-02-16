549600f6e03802376131db1763ecb3eadf70fb6b
Felipe <97138732+rendeviluke@users.noreply.github.com>
2024-02-16 11:05:02 -0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,85 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+
+function checkAnswer(currentLevel){
+    console.log(gamePattern + "\n" + userClickedPattern + "\n" + level + "\n" + started);
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        console.log("Success");
+        if (userClickedPattern.length === gamePattern.length){
+            setTimeout(function(){
+                nextSequence();
+            }, 1000)
+        }
+    } else {
+        var wrong_sound = new Audio('./sounds/wrong.mp3');
+        wrong_sound.play();
+        $("h1#level-title").text("Game Over, Press Any Key to Restart");
+        $("body").addClass("game-over");
+
+        setTimeout(function(){
+        $("body").removeClass("game-over");
+
+        }, 200)
+        console.log("Wrong");
+        startOver();
+    }
+}
+
+function startOver(){
+    level = 0;
+    started = false;
+    gamePattern = [];
+}
+
+
+$(document).keydown(function(){
+    if(!started){
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+
+});
+
+// user pick a color here
+$(".btn").click(function(){
+
+    var userChosenColour = $(this).attr("id");
+
+    userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length-1);
+});
+
+function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level "+ level);
+
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+
+
+}
+function playSound(name) {
+    var audio = new Audio("./sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function(){
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
